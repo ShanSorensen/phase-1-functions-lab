e5642c53551c944588a77a86fd00178f06ff0a29
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-25 08:57:59 -0500

    commit tree 7abcb96aaaac0ae1fa5f224303d8fa12f90efaca

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block1, block2) {
+    if(block1 > block2){
+        return (block1 - block2)
+    } else if (block1 < block2){
+        return (block2 - block1)
+    }
+}
+
+
+function distanceFromHqInFeet(blocks) {
+    let distance = distanceFromHqInBlocks(blocks);
+    return distance * 264
+};
+
+
+
+
+function distanceTravelledInFeet(block1, block2){
+    if(block1>block2){
+        return (block1-block2) * 264
+    } else{
+        return (block2-block1) * 264
+    };
+};
+
+function calculatesFarePrice(block1, block2){
+    let feetDistance = distanceTravelledInFeet(block1, block2);
+
+    if(feetDistance<400){
+        return 0
+    } else if(feetDistance>=400 && feetDistance<=2000){
+        return 0.02 * (feetDistance-400)
+    } else if(feetDistance>2000 && feetDistance<2500){ 
+        return 25
+    } else {
+        return `cannot travel that far`
+    }
+
+}
